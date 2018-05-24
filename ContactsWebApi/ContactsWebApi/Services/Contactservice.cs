3e3fe1a1f97b4a5d3145f87c0959dea36ff6fc17
kallepakarinen <cuttu85@gmail.com>
2018-05-24 12:10:50 +0300

    adding controllers, repositories and services

---
+++ ContactsWebApi/ContactsWebApi/Services/Contactservice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactsWebApi.Models;
+using ContactsWebApi.Repositories;
+
+namespace ContactsWebApi.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly IContactRepository _contactRepository;
+
+        public ContactService(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
+
+        public List<Contact> GetContacts()
+        {
+            return _contactRepository.Get();
+        }
+
+        public Contact GetContactByID(int id)
+        {
+            return _contactRepository.Get(id);
+        }
+
+    }
+}
